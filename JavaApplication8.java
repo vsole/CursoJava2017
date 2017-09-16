95964987dc90b4328ddcf2ff27789f423a2ad697
vsole <31908760+vsole@users.noreply.github.com>
2017-09-16 10:14:29 -0300

    Ejercicio 8
    Superficie de un terreno.

---
+++ JavaApplication8.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication8;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Valeria
+ */
+public class JavaApplication8 {
+
+    /**
+     * @param args the command line arguments
+     * Se pide el largo y ancho de un terreno u se pide la superficie del mismo.
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float Num1;
+        float Num2;
+        float Num3;
+        
+        Scanner traerDato=new scanner (System.in);
+        
+         System.out.println("Si el largo de un terreno es:");
+         num1= traerDato.nextFloat();
+         System.out.println("Y el ancho es:");
+         num2= traerDato.nextFloat();
+    }
+    
+}
