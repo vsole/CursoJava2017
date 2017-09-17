d7ff23de94b5bbc3d0b024cdadddf1ca81eb6cab
Valeria Soricetti <31908760+vsole@users.noreply.github.com>
2017-09-17 11:19:52 -0300

    Ejercicio 8
    perimetro de un terreno

--- JavaApplication8.java
+++ JavaApplication8.java
@@ -19,16 +19,18 @@ public class JavaApplication8 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        float Num1;
-        float Num2;
-        float Num3;
+        float num1;
+        float num2;
+        float num3;
         
-        Scanner traerDato=new scanner (System.in);
+        Scanner traerDato= new Scanner(System.in);
         
          System.out.println("Si el largo de un terreno es:");
          num1= traerDato.nextFloat();
          System.out.println("Y el ancho es:");
          num2= traerDato.nextFloat();
+         num3= num1*num2;
+         System.out.println("Su superficie es:"+ num3);    
     }
     
 }
